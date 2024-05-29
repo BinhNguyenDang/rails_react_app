dbe1b5dfe804680ebe04f28df938f97d8e6aeead
Binh Nguyen Dang <binhnguyendang2003@gmail.com>
2024-05-29 17:42:23 +0700

    Refactor into services

--- client/src/features/posts/PostDetails.jsx
+++ client/src/features/posts/PostDetails.jsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from "react";
 import { useParams, useNavigate, Link } from "react-router-dom";
-import { API_URL } from "../../constants";
+import { fetchPost, deletePost as deletePostService } from "../../services/postService";
 
 function PostDetails(){
     const [post, setPost] = useState(null);
@@ -11,13 +11,8 @@ function PostDetails(){
     useEffect(() => {
         const fetchCurrentPost = async () => {
             try {
-                const response = await fetch(`${API_URL}/${id}`);
-                if (response.ok) {
-                    const json = await response.json();
-                    setPost(json);
-            } else {
-                throw response;
-            }
+                const json = await fetchPost(id);
+                setPost(json);
         } catch (err) {
             console.log("An error occurred:", err);
         }
@@ -27,16 +22,10 @@ function PostDetails(){
 
     const deletePost = async () => {
         try {
-            const response = await fetch(`${API_URL}/${id}`, {
-                method: "DELETE",
-            });
-            if (response.ok) {
-                navigate("/");
-            } else {
-                throw response;
-            }
+            await deletePostService(post.id);
+            navigate("/");
         } catch (err) {
-            console.error(err);
+            console.error("Failed to delete the post: ",err);
         }
     }
 
